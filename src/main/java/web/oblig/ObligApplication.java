2b9ebd26383ea665540ce454d6181dc8d12e5456
herman bjørnerud <herman.bjorn@gmail.com>
2020-03-08 21:43:06 +0100

    initial upload

---
+++ src/main/java/web/oblig/ObligApplication.java
@@ -0,0 +1,13 @@
+package web.oblig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObligApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ObligApplication.class, args);
+    }
+
+}
---
+++ src/test/java/web/oblig/ObligApplicationTests.java
@@ -0,0 +1,13 @@
+package web.oblig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObligApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
